46a32062d273589f61e6e1f747985e3a31af9e4a
Richard Marmorstein <richardm@stripe.com>
2023-07-17 09:54:29 -0700

    Manually remove six from tests/test_util.py

--- tests/test_util.py
+++ tests/test_util.py
@@ -5,10 +5,6 @@ from collections import namedtuple
 
 import stripe
 from stripe import util
-from stripe.six import ensure_text
-from stripe.six.moves import builtins
-
-PRINT_FUNC_STRING = builtins.__name__ + ".print"
 
 LogTestCase = namedtuple("LogTestCase", "env flag should_output")
 FmtTestCase = namedtuple("FmtTestCase", "props expected")
@@ -53,7 +49,7 @@ class TestUtil(object):
         for case in test_cases:
             try:
                 logger_mock = mocker.patch(logger_name)
-                print_mock = mocker.patch(PRINT_FUNC_STRING)
+                print_mock = mocker.patch('builtins.print')
                 mocker.patch("stripe.log", case.flag)
                 mocker.patch("stripe.util.STRIPE_LOG", case.env)
 
@@ -155,5 +151,6 @@ class TestUtil(object):
 
     def test_sanitize_id(self):
         sanitized_id = util.sanitize_id("cu  %x 123")
-        assert ensure_text(sanitized_id)
+        if isinstance(sanitized_id, bytes):
+            sanitized_id = sanitized_id.decode("utf-8", 'strict')
         assert sanitized_id == "cu++%25x+123"
